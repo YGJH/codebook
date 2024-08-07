51ef1a7d89eb2de93f08724e418fd7dd7c34613b
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2024-08-08 00:23:51 +0800

    update point

--- dataStructure/fenwickTree.cpp
+++ dataStructure/fenwickTree.cpp
@@ -3,23 +3,20 @@ struct fenwick{
 	int n;
 	vector<int> v;
 	fenwick(int _n) : n(_n+1),v(_n+2){}
-	void update(int x,int u){
+	void add(int x,int u){
 		++x;
 		for(;x < n; x += lowbit(x)) v[x] += u;
 	}
-	int query(int x){
+	int qry(int x){
 		++x; int ret = 0;
 		for(; x ; x -= lowbit(x)) ret += v[x];
 		return ret;
 	}
-	int query(int l,int r) { return query(r) - query(l-1); }
+	int qry(int l,int r) { return query(r) - query(l-1); }
 	int kth(int k){ // lower_bound(k)
-		int x = 0;
-		--k;
+		int x = 0; --k;
 		for(int i = (1<<__lg(n)); i;i >>= 1){
-			if(x + i <= n and k >= v[x + i]){
-				x += i; k -= v[x];
-			}
+			if(x + i <= n and k >= v[x + i]) x += i; k -= v[x];
 		}
 		return x;
 	}
--- geometry/definition.cpp
+++ geometry/definition.cpp
@@ -1,28 +1,30 @@
-#define pi acos(-1)
-using ld = long double;
-const ld eps = 1e-8;
-int dcmp(ld x) {
-  if(abs(x) < eps) return 0;
-  else return x < 0 ? -1 : 1;
-}
-
 template<class T>
 struct pt{
 	T x,y;
 	pt(T _x,T _y):x(_x),y(_y){}
 	pt():x(0),y(0){}
 	
-	pt operator * (const T  &c) const { return pt(x*c,y*c);}
-	pt operator / (const T  &c) const { return pt(x/c,y/c);}
-	pt operator + (const pt &a) const { return pt(x+a.x,y+a.y);}
-	pt operator - (const pt &a) const { return pt(x-a.x,y-a.y);}
-	T  operator * (const pt &a) const { return x*a.x + y*a.y;}
-	T  operator ^ (const pt &a) const { return x*a.y - y*a.x;}
+	pt operator * (T  c){ return pt(x*c,y*c);}
+	pt operator / (T  c){ return pt(x/c,y/c);}
+	pt operator + (pt a){ return pt(x+a.x,y+a.y);}
+	pt operator - (pt a){ return pt(x-a.x,y-a.y);}
+	T  operator * (pt a){ return x*a.x + y*a.y;}
+	T  operator ^ (pt a){ return x*a.y - y*a.x;}
 
-	bool operator < (const pt &a) const { return x < a.x || (x == a.x && y < a.y);};
-	bool operator == (const pt &a) const { return dcmp(x-a.x) && dcmp(y-a.y);};
+	auto operator<=>(pt o) const { return (x != o.x) ? x <=> o.x : y <=> o.y; }
+	bool operator < (pt a) const { return x < a.x || (x == a.x && y < a.y);};
+	bool operator== (pt a) const { return x == a.x and y == a.y;};
 };
+
+using numbers::pi;
+using ld = long double;
+const ld eps = 1e-8L;
 using Pt = pt<ld>;
+
+int dcmp(ld x) { return (x > -eps) - (x < eps); }
+ld ori(Pt a, Pt b, Pt c) { return (b - a) ^ (c - a); }
+ld abs(Pt a) { return sqrt(a * a); }
+ld abs2(Pt a) { return a * a; }
 
 istream &operator>>(istream &s, Pt &a) { return s >> a.x >> a.y; }
 ostream &operator<<(ostream &s, Pt &a) { return s << "(" << a.x << ", " << a.y << ")";} 
