3a1deff5a274c417dd542642060f52cf03af8842
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2024-09-10 02:44:52 +0800

    remove io

--- basic/default.cpp
+++ basic/default.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 #define masterspark ios::sync_with_stdio(0), cin.tie(0),cout.tie(0),cin.exceptions(cin.failbit);
 
-template<class F, class S> ostream &operator<<(ostream &s, pair<F, S> &v) { return s << "(" << v.first << ", " << v.second << ")";} 
-template<class F, class S> istream &operator>>(istream &s, pair<F, S> &v) { return s >> v.first >> v.second; }
-template<class T> istream &operator>>(istream &s, vector<T> &a) { for (auto &x:a) s>>x; return s; }
-template<class T> ostream &operator<<(ostream &s, vector<T> &a) { int n=a.size(); if (!n) return s; s<<a[0]; for (int i=1; i<n; i++) s<<' '<<a[i]; return s; }
- 
 #define int long long
 #define pp pair<int, int>
 #define ff first
@@ -20,9 +15,9 @@ template<class T> ostream &operator<<(ostream &s, vector<T> &a) { int n=a.size()
 #define all(v) (v).begin(), (v).end()
 #define FZ(x) memset(x, 0, sizeof(x)) //fill zero
 #define SZ(x) ((int)x.size())
-using i128 = __int128;
-using i64 = __int64;
-using i32 = __int32;
+using i128 = __int128_t;
+using i64 = __int64_t;
+using i32 = __int32_t;
 
 void solve(){
     
